24f61b1911be7013d0b4ef79c745aa656ef826c5
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-07 12:57:10 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_6-Operaciones Básicas/Ejercicio6.java
@@ -0,0 +1,23 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo
+//Día: 07/02/2019
+//Hora: 12:39
+import java.util.Scanner;
+class Ejercicio6{
+public static void main(String[]args){
+Scanner Num = new Scanner(System.in);
+float x,y,S,R,M,D,Md;
+System.out.println("Inserta el primer numero");
+x=Num.nextInt();
+System.out.println("Inserta el segundo numero");
+y=Num.nextInt();
+S=x+y;
+R=x-y;
+M=x*y;
+D=x/y;
+Md=x%y;
+System.out.println(x+" + "+y+" = "+S);
+System.out.println(x+" - "+y+" = "+R);
+System.out.println(x+" * "+y+" = "+M);
+System.out.println(x+"/"+y+" = "+D);
+System.out.println(x+" mod "+y+" = "+Md);
+}}
