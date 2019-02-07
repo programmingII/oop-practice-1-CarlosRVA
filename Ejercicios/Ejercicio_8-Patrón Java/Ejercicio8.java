2da9de0c8f8301eacc4dbd292dbbba741aaecb3f
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-07 13:25:50 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_8-Patrón Java/Ejercicio8.java
@@ -0,0 +1,10 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo
+//Día: 07/02/2019
+//Hora: 13:19
+class Ejercicio8{
+public static void main(String[]args){
+System.out.println("   J     a    v     v   a  ");
+System.out.println("   J    a a    v   v   a a ");
+System.out.println("J  J   aaaaa    V V   aaaaa ");
+System.out.println(" JJ   a     a    V   a     a ");
+}}
