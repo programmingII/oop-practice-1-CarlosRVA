37e1df693a69d2e7d8ab659b42ab2f26186a5bea
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-08 16:39:32 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_1-Helloworld/Ejercicio1.java
@@ -0,0 +1,9 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 05/02/2019
+//Hora: 12:24
+class Ejercicio1{
+public static void main(String[]args){
+System.out.println("Hello");
+System.out.println("Carlos Villanueva");
+System.out.println("Matricula: 169881");
+}}
