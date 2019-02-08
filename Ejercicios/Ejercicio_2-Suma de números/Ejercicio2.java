e275915d8dffac44d48dbc4fb5daff14ba23c731
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-08 16:43:16 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_2-Suma de números/Ejercicio2.java
@@ -0,0 +1,8 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 05/02/2019
+//Hora: 12:56
+class Ejercicio2{
+public static void main(String[]args){
+System.out.println(74+36);
+System.out.println(252+200+58);
+}}
