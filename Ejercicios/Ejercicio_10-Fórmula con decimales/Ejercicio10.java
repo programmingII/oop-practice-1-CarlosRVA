49bf7674ba9258206ec436c7265791eb6f47b96c
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-09 10:59:16 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_10-Fórmula con decimales/Ejercicio10.java
@@ -0,0 +1,11 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 09/02/2019
+//Hora: 10:44
+class Ejercicio10{
+public static void main(String[]args){
+double A,B,C;
+A=4.0;
+B=(6.0-(6.0/7)+(6.0/2)-(6.0/8)+(6.0/9)-(6.0/15)); //Poner el dividendo como decimal para evitar que el programa lo redondee
+C=A*B;
+System.out.println(C);
+}}
