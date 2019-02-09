88ef828340857bc1490a43c175bdd3486e128b87
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-09 10:14:26 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_9-Operación con decimales/Ejercicio9.java
@@ -0,0 +1,11 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 09/02/2019
+//Hora: 10:00
+class Ejercicio9{
+public static void main(String[]args){
+double A,B,C;
+A=(32.2*6.6-6.6*6.6);
+B=(20.1-2.1);
+C=A/B;
+System.out.println(C);
+}}
