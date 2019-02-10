7957f6be58daf7f7cee1668d598bc080503a910f
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-10 09:34:42 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_11-Área y Perímetro de un Circulo/Ejercicio11.java
@@ -0,0 +1,13 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 10/02/2019
+//Hora: 09:19
+import java.util.Scanner;
+class Ejercicio11{
+public static void main(String[]args){
+Scanner Rad = new Scanner(System.in);
+double R,P,A;
+System.out.println("Introduce el radio del circulo");
+R=Rad.nextDouble();
+System.out.println("El perimetro es: "+R*2*Math.PI);
+System.out.println("El area es: "+R*R*Math.PI);
+}}
