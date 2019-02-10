5874d4605c0dab2e41a8e5376b431a75a38d4b64
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-10 14:03:26 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_16-Imprimir Cara/Ejercicio16.java
@@ -0,0 +1,11 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 10/02/2019
+//Hora: 13:28
+class Ejercicio16{
+public static void main(String[]args){
+System.out.println(" +\"\"\"\"\"+ "); //Se inserta una barra invertida para que las comillas se muestren como texto
+System.out.println("[| o o |]");
+System.out.println(" |  ^  |");
+System.out.println(" | '-' |");
+System.out.println(" +-----+");
+}}
