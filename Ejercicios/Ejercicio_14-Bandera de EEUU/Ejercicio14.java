89d3e9744dfc9d69a40274e90f1029431ebd8c49
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-10 10:23:20 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_14-Bandera de EEUU/Ejercicio14.java
@@ -0,0 +1,15 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 10/02/2019
+//Hora: 10:13
+class Ejercicio14{
+public static void main(String[]args){
+String[] A = new String[11];
+for(int x=1;x<=4;x++){
+System.out.println("* * * * * * ==================================");
+System.out.println(" * * * * *  ==================================");
+}
+System.out.println("* * * * * * ==================================");
+for(int x=5;x<=10;x++){
+System.out.println("==============================================");
+}
+}}
