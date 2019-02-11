dcad342e53c85a8eb646b7de5e3c43795abdcf95
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-11 12:55:34 -0700

    Update Ejercicio10.java

--- Ejercicios/Ejercicio_10-Fórmula con decimales/Ejercicio10.java
+++ Ejercicios/Ejercicio_10-Fórmula con decimales/Ejercicio10.java
@@ -1,11 +1,13 @@
 //Nombre: Villanueva Aguilar Carlos Raymundo 
-//D韆: 09/02/2019
+//D铆a: 09/02/2019
 //Hora: 10:44
+//La clase Ejercicio10 implementa una aplicaci贸n que realiza una f贸rmula con decimales e imprime el resultado.
 class Ejercicio10{
 public static void main(String[]args){
-double A,B,C;
-A=4.0;
-B=(6.0-(6.0/7)+(6.0/2)-(6.0/8)+(6.0/9)-(6.0/15)); //Poner el dividendo como decimal para evitar que el programa lo redondee
-C=A*B;
-System.out.println(C);
+double A,B,C;//Se declaran las variables como tipo double para poder trabajar con decimales.
+A=4.0;//Se declara la variable A como 4.0, es importante poner el punto decimal para que el programa sepa que se va a trabajar con ellos.
+B=(6.0-(6.0/7)+(6.0/2)-(6.0/8)+(6.0/9)-(6.0/15)); /*Se declara la variable B, es importante poner el dividendo como decimal
+para evitar que el programa redondee el resultado. La variable almacenar谩 el resultado de esta operaci贸n.*/
+C=A*B;//Se declara la variable C como el resultado de la multiplicaci贸n entre las otras dos variables.
+System.out.println(C);//Se imprime la variable C, la cual es el resultado de la multiplicaci贸n anterior.
 }}
