8b8060e2ebb251d92137aa3450dc0caacaac7930
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-11 12:31:14 -0700

    Update Ejercicio6.java

--- Ejercicios/Ejercicio_6-Operaciones Básicas/Ejercicio6.java
+++ Ejercicios/Ejercicio_6-Operaciones Básicas/Ejercicio6.java
@@ -1,23 +1,25 @@
 //Nombre: Villanueva Aguilar Carlos Raymundo
 //Día: 07/02/2019
 //Hora: 12:39
-import java.util.Scanner;
+import java.util.Scanner;//Importa las funciones necesarias para la entrada de datos
+//La clase Ejercicio6 implementa una aplicación que imprime el resultado de varias operaciones básicas
 class Ejercicio6{
 public static void main(String[]args){
-Scanner Num = new Scanner(System.in);
-float x,y,S,R,M,D,Md; //Se declaran como float para que se muestren decimales al dividir.
-System.out.println("Inserta el primer numero");
-x=Num.nextInt();
-System.out.println("Inserta el segundo numero");
-y=Num.nextInt();
-S=x+y;
-R=x-y;
-M=x*y;
-D=x/y;
-Md=x%y;
-System.out.println(x+" + "+y+" = "+S);
-System.out.println(x+" - "+y+" = "+R);
-System.out.println(x+" * "+y+" = "+M);
-System.out.println(x+"/"+y+" = "+D);
-System.out.println(x+" mod "+y+" = "+Md);
+Scanner Num = new Scanner(System.in);/*La función crea un Scanner para poder introducir datos.
+Después de la palabra "Scanner" se declara el nombre del mismo.*/
+float x,y,S,R,M,D,Md; //Se declaran las variables como float para que se muestren decimales al dividir.
+System.out.println("Inserta el primer numero");//Se utiliza el comando para imprimir en pantalla.
+x=Num.nextInt();//Esta función llama al scanner para leer el próximo número entero que se introduzca.
+System.out.println("Inserta el segundo numero");//Se vuelve a usar el comando para pedir al usuario que introduzca un segundo numero
+y=Num.nextInt();//Se vuelve a llamar al scanner para leer otro número entero.
+S=x+y;//Se declara "S" como la suma de la variable x y la variable y.
+R=x-y;//Se declara "R" como la resta de las variables.
+M=x*y;//Se declara "M" como el resultado de la multiplicación de las variables.
+D=x/y;//Se declara "D" como el resultado de la división de las variables.
+Md=x%y;//Se declara "Md" como el residuo de la división de las variables.
+System.out.println(x+" + "+y+" = "+S);//Se muestra el proceso de la suma y el resultado de esta.
+System.out.println(x+" - "+y+" = "+R);//Se muestra el proceso de la resta y el resultado de esta.
+System.out.println(x+" * "+y+" = "+M);//Se muestra el proceso de la multiplicación y el resultado de esta.
+System.out.println(x+"/"+y+" = "+D);//Se muestra el proceso de la división y el resultado de esta.
+System.out.println(x+" mod "+y+" = "+Md);//Se muestra el residuo de la división entre las variables.
 }}
