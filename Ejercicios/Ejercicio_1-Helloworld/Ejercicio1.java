c9be777284d08488c85491ed80fb3bac32ea99c3
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-10 18:09:06 -0700

    Update Ejercicio1.java

--- Ejercicios/Ejercicio_1-Helloworld/Ejercicio1.java
+++ Ejercicios/Ejercicio_1-Helloworld/Ejercicio1.java
@@ -1,8 +1,8 @@
 //Nombre: Villanueva Aguilar Carlos Raymundo 
 //Día: 05/02/2019
 //Hora: 12:24
-class Ejercicio1{/*Se crea una clase para poder tener un "espacio" en donde trabajar
-public static void main(String[]args){//Se escribe main para que el programa se pueda ejecutar.
+class Ejercicio1{//Se crea una clase para poder tener un "espacio" en donde trabajar
+public static void main(String[]args){/*Se escribe main para que el programa se pueda ejecutar.
 Se le agregan atributos como public o void para definir sus valores de retorno o su disponibilidad de uso ante otras clases.*/
 System.out.println("Hello");//Este comando se utiliza para mostrar algo en pantalla.
 System.out.println("Carlos Villanueva");//Si se quiere mostrar un texto en particular se introduce entre comillas
