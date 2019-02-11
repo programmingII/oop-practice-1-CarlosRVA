579e8728762049246d53691deea5d73e59ed6e06
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-10 18:50:07 -0700

    Update Ejercicio2.java

--- Ejercicios/Ejercicio_2-Suma de números/Ejercicio2.java
+++ Ejercicios/Ejercicio_2-Suma de números/Ejercicio2.java
@@ -1,8 +1,9 @@
 //Nombre: Villanueva Aguilar Carlos Raymundo 
-//D�a: 05/02/2019
+//Día: 05/02/2019
 //Hora: 12:56
-class Ejercicio2{
-public static void main(String[]args){
-System.out.println(74+36);
-System.out.println(252+200+58);
+class Ejercicio2{//Se crea la clase para poder tener un lugar en donde trabajar.
+public static void main(String[]args){//Se crea la clase main para poder ejecutar el programa y se agregan los atributos.
+System.out.println(74+36);//Se utiliza el compando para mostrar datos en pantalla.
+System.out.println(252+200+58);/* Además, es capaz de procesar datos numéricos y mostrar
+los resultados de operaciones entre dichos datos.*/
 }}
