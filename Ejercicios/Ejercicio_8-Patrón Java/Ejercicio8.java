0298c0c6746f7b5254f8ff0360234e24fb8e9a76
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-11 12:46:57 -0700

    Update Ejercicio8.java

--- Ejercicios/Ejercicio_8-Patrón Java/Ejercicio8.java
+++ Ejercicios/Ejercicio_8-Patrón Java/Ejercicio8.java
@@ -1,10 +1,12 @@
 //Nombre: Villanueva Aguilar Carlos Raymundo
-//D�a: 07/02/2019
+//Día: 07/02/2019
 //Hora: 13:19
+//La clase Ejercicio8 implementa una aplicación que imprime texto con un patron establecido para formar una palabra.
 class Ejercicio8{
 public static void main(String[]args){
-System.out.println("   J     a    v     v   a  ");
-System.out.println("   J    a a    v   v   a a ");
-System.out.println("J  J   aaaaa    V V   aaaaa ");
-System.out.println(" JJ   a     a    V   a     a ");
+System.out.println("   J     a    v     v   a  ");//Se utiliza el comando para imprimir en pantalla
+System.out.println("   J    a a    v   v   a a ");//Se debe recordar poner el texto entre comillas
+System.out.println("J  J   aaaaa    V V   aaaaa ");//El texto va posicionado de una manera específica
+System.out.println(" JJ   a     a    V   a     a ");/*Si se posiciona correctamente, el programa deberá imprimir la palabra
+"JAVA", donde cada letra de la palabra está formada por esa misma letra pero posicionada de una manera específica*/
 }}
