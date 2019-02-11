1c1eacb1599c6ad14f0d2ba7aac03f83aeed474a
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-10 18:08:03 -0700

    Update Ejercicio1.java

--- Ejercicios/Ejercicio_1-Helloworld/Ejercicio1.java
+++ Ejercicios/Ejercicio_1-Helloworld/Ejercicio1.java
@@ -1,9 +1,10 @@
 //Nombre: Villanueva Aguilar Carlos Raymundo 
-//D�a: 05/02/2019
+//Día: 05/02/2019
 //Hora: 12:24
-class Ejercicio1{
-public static void main(String[]args){
-System.out.println("Hello");
-System.out.println("Carlos Villanueva");
-System.out.println("Matricula: 169881");
+class Ejercicio1{/*Se crea una clase para poder tener un "espacio" en donde trabajar
+public static void main(String[]args){//Se escribe main para que el programa se pueda ejecutar.
+Se le agregan atributos como public o void para definir sus valores de retorno o su disponibilidad de uso ante otras clases.*/
+System.out.println("Hello");//Este comando se utiliza para mostrar algo en pantalla.
+System.out.println("Carlos Villanueva");//Si se quiere mostrar un texto en particular se introduce entre comillas
+System.out.println("Matricula: 169881");//En caso de no llevar comillas, el compilador lo considerará como una o varias variables.
 }}
