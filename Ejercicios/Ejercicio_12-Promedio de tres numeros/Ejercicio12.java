aa07d0afcda88e64000fd00474f60fec2a562259
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-11 17:33:48 -0700

    Update Ejercicio12.java

--- Ejercicios/Ejercicio_12-Promedio de tres numeros/Ejercicio12.java
+++ Ejercicios/Ejercicio_12-Promedio de tres numeros/Ejercicio12.java
@@ -1,17 +1,19 @@
 //Nombre: Villanueva Aguilar Carlos Raymundo 
-//D韆: 10/02/2019
+//D铆a: 10/02/2019
 //Hora: 09:38
-import java.util.Scanner;
+import java.util.Scanner;//Importa las funciones de scanner que se usan para la entrada de datos.
 class Ejercicio12{
 public static void main(String[]args){
-Scanner N = new Scanner(System.in);
-int A,B,C,S;
-System.out.println("Introduce el primer numero");
-A=N.nextInt();
-System.out.println("Introduce el segundo numero");
-B=N.nextInt();
-System.out.println("Introduce el tercer numero");
-C=N.nextInt();
-S=A+B+C;
-System.out.println("El promedio de los tres numeros es: "+S/3.0);
+Scanner N = new Scanner(System.in);//Se crea un nuevo scanner y se le asigna el nombre de "N".
+int A,B,C,S;//Se crean 4 variables de tipo int.
+System.out.println("Introduce el primer numero");//Se usa el comando para imprimir en pantalla y se escribe una instrucci贸n.
+A=N.nextInt();//Llama al scanner para capturar el pr贸ximo n煤mero entero que se inserte y asignarlo como valor de A.
+System.out.println("Introduce el segundo numero");//Se utiliza el mismo comando para imprimir una segunda instrucci贸n.
+B=N.nextInt();//Se vuelve a llamar al scanner para realizar la misma tarea con el valor de B.
+System.out.println("Introduce el tercer numero");//Se vuelve a utilizar el comando para mostrar una tercera instrucci贸n.
+C=N.nextInt();//Se llama al scanner una vez mas para el valor de C.
+S=A+B+C;//Se realiza la suma de A, B y C, y el resultado se asigna como valor de S.
+System.out.println("El promedio de los tres numeros es: "+S/3.0);/*Se utiliza de nuevo el comando para imprimir el promedio
+en pantalla. Se divide S entre la cantidad de valores sumados (3: A,B,C), y se le agrega punto decimal al 3 para que el resultado 
+muestre decimales en caso de ser necesario.
 }}
