7f775b9d9a3c884c3972f5f06a181773ff006522
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-12 16:39:07 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_18-Multiplicación de dos números binarios/Ejercicio18.java
@@ -0,0 +1,16 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 12/02/2019
+//Hora: 16:23
+import java.util.Scanner;
+class Ejercicio18{
+public static void main(String[]args){
+String x,y;
+Scanner num = new Scanner(System.in);
+System.out.println("Inserta el primer numero binario");
+x = num.nextLine();
+System.out.println("Inserta el primer numero binario");
+y = num.nextLine();
+System.out.println("El resultado de la multiplicación de estos dos binarios es: "
++(Integer.toBinaryString(Integer.parseInt(x,2)*Integer.parseInt(y,2))));
+
+}}
