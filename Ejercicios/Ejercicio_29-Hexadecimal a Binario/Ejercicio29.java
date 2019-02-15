b9626a0bf21fecdf94990d49f9f99600c3a7ad7d
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-15 15:21:08 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_29-Hexadecimal a Binario/Ejercicio29.java
@@ -0,0 +1,13 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 15/02/2019
+//Hora: 13:18
+import java.util.Scanner;
+class Ejercicio29{
+public static void main(String[]args){
+Scanner Hex = new Scanner(System.in);
+String x;
+System.out.println("Inserta un numero en hexadecimal");
+x = Hex.nextLine();
+System.out.println("Al convertirse a binario, el numero es: "
++ Integer.toString(Integer.parseInt(x,16),2));
+}}
