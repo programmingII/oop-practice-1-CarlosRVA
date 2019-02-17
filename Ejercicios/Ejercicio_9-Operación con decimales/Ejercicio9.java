f4f9300b45c075f9564211ab6ccf1edec5343fcc
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-17 16:11:23 -0700

    Update Ejercicio9.java

--- Ejercicios/Ejercicio_9-Operación con decimales/Ejercicio9.java
+++ Ejercicios/Ejercicio_9-Operación con decimales/Ejercicio9.java
@@ -3,7 +3,8 @@
 //Hora: 10:00
 //La clase Ejercicio9 implementa una aplicación que realiza una operación e imprime el resultado con números decimales.
 class Ejercicio9{
-public static void main(String[]args){
+public static void main(String[]args){/*Se crea el metodo main para poder ejecutar el programa y se le agregan los atributos
+de disponibilidad de uso, argumentos y valores de retorno.*/
 double A,B,C;//Se declaran las variables de tipo double para que se pueda trabajar con decimales.
 A=(32.2*6.6-6.6*6.6);/*Se declara A como la primera parte de la división a realizar, dentro de A se hace una operación y
 se almacena el resultado.*/
