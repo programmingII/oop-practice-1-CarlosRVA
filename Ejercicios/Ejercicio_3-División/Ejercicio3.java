81eef9f58a60c7722f5b560d7eee792c397d2d17
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-17 14:39:56 -0700

    Update Ejercicio3.java

--- Ejercicios/Ejercicio_3-División/Ejercicio3.java
+++ Ejercicios/Ejercicio_3-División/Ejercicio3.java
@@ -3,7 +3,7 @@
 //Hora: 13:02
 /*La clase Ejercicio3 implementa una aplicación que imprime el resultado de una división*/
 class Ejercicio3{
-public static void main(String[]args){/*Se crea la clase que permite la ejecución del programa y se le agregan los
+public static void main(String[]args){/*Se crea el método que permite la ejecución del programa y se le agregan los
 atributos de disponibilidad de uso, argumentos y valores de retorno.*/
 System.out.println(3000/75);/*Se utiliza el comando que abre el flujo de salida y recibe datos para luego imprimirlos en pantalla,
 y se escribe la división dentro del paréntesis. El resultado de la división se mostrará al ejecutar el programa.*/
