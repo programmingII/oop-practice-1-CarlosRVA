d233f5ca8923dc1b59ec7de2471b579f0375e58e
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-17 10:40:30 -0700

    Update Ejercicio1.java

--- Ejercicios/Ejercicio_1-Helloworld/Ejercicio1.java
+++ Ejercicios/Ejercicio_1-Helloworld/Ejercicio1.java
@@ -3,8 +3,12 @@
 //Hora: 12:24
 //La clase Ejercicio1 implementa una aplicación que imprime texto en pantalla.
 class Ejercicio1{
-public static void main(String[]args){/*Se escribe main para que el programa se pueda ejecutar.
-Se le agregan atributos como public o void para definir sus valores de retorno o su disponibilidad de uso ante otras clases.*/
+public static void main(String[]args){/*Se escribe el método main para que el programa se pueda ejecutar. Cuando se inicia un programa,
+se busca automáticamente el método main.
+Se le agregan atributos como public para definir su disponibilidad de uso ante otras clases, static para indicar que no hay objetos
+presentes en el método y que se pueda cargar la clase en memoria para llamar al método,void se usapara definir que no tiene un valor
+de retorno. Finalmente, String[]args indica que acepta argumentos en modo de string, esto quiere decir que el texto escrito dentro del
+método se identifica como argumento.*/
 System.out.println("Hello");/*Este comando se utiliza para imprimir en pantalla. Se abre un flujo de salida que está listo para
 aceptar e imprimir datos*/
 System.out.println("Carlos Villanueva");//Si se quiere mostrar un texto en particular se introduce entre comillas
