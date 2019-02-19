6a74a84c4178dfe65f615b2c980705d1524a2006
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-19 08:53:51 -0700

    Update Ejercicio21.java

--- Ejercicios/Ejercicio_21-Decimal a Octal/Ejercicio21.java
+++ Ejercicios/Ejercicio_21-Decimal a Octal/Ejercicio21.java
@@ -1,13 +1,17 @@
 //Nombre: Villanueva Aguilar Carlos Raymundo 
-//D�a: 15/02/2019
+//Día: 15/02/2019
 //Hora: 09:24
-import java.util.Scanner;
+import java.util.Scanner;//Se importa la librería scanner para poder usar funciones de entrada de datos.
+//La clase Ejercicio21 implementa una aplicación que recibe un número decimal, lo convierte a octal y lo imprime en pantalla.
 class Ejercicio21{
-public static void main(String[]args){
-Scanner num = new Scanner(System.in);
-String x;
-System.out.println("Inserta un numero decimal");
-x=num.nextLine();
+public static void main(String[]args){/*Se escribe el metodo main para poder ejecutar el programa y se asignan los atributos de
+argumentos, valor de retorno y disponibilidad de uso.*/
+Scanner num = new Scanner(System.in);//Se crea un nuevo scanner y se le asigna el nombre de "num".
+String x;//Se declara una nueva variable tipo string
+System.out.println("Inserta un numero decimal");//Se utiliza el comando para imprimir en pantalla y se escribe una instrucción.
+x=num.nextLine();//Se llama al scanner para almacenar en x el texto que se inserte en la próxima línea.
 System.out.println("Al convertirse a octal, el numero es: "+
 Integer.toString(Integer.parseInt(x,10),8));
+/*El texto escrito es convertido a número decimal utilizando Integer.parseInt, luego es convertido a octal con Integer.toString y
+finalmente es impreso en pantalla.*/
 }}
