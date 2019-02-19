fe60c5f3188de4b7918e0592b91aa7b31f53852e
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-19 09:17:51 -0700

    Update Ejercicio26.java

--- Ejercicios/Ejercicio_26-Octal a Binario/Ejercicio26.java
+++ Ejercicios/Ejercicio_26-Octal a Binario/Ejercicio26.java
@@ -1,13 +1,17 @@
 //Nombre: Villanueva Aguilar Carlos Raymundo 
-//D�a: 15/02/2019
+//Día: 15/02/2019
 //Hora: 14:52
-import java.util.Scanner;
+import java.util.Scanner;//Se importa la librería scanner para usar funciones de entrada de datos.
+//La clase Ejercicio26 implementa una aplicación que recibe un número en octal, lo convierte a binario y lo imprime en pantalla.
 class Ejercicio26{
-public static void main(String[]args){
-Scanner Oct = new Scanner(System.in);
-String x;
-System.out.println("Escribe un numero en octal");
-x = Oct.nextLine();
+public static void main(String[]args){/*Se escribe el método main para ejecutar el programa y se le asignan atributos como la
+disponibilidad de uso, valor de retorno y argumentos.*/
+Scanner Oct = new Scanner(System.in);//Se crea un nuevo scanner con el nombre de "Oct".
+String x;//Se declara una nueva variable tipo string.
+System.out.println("Escribe un numero en octal");//Se utiliza el comando para imprimir en pantalla y se escribe una instrucción.
+x = Oct.nextLine();//Se llama al scanner para almacenar en x a la próxima línea de texto que se inserte.
 System.out.println("Al convertirse a binario, el numero es: "
 + Integer.toString(Integer.parseInt(x,8),2));
+/*El texto es interpretado como octal y convertido a número con Integer.parseInt, es convertido a binario con Integer.toString y finalmente
+es impreso en pantalla con Integer.toString.*/
 }}
