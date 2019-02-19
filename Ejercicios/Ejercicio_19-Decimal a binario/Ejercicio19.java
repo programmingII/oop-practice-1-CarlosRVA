5d895a67e876bb9d9ce912e7ac69b493b2915b08
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-19 08:25:53 -0700

    Update Ejercicio19.java

--- Ejercicios/Ejercicio_19-Decimal a binario/Ejercicio19.java
+++ Ejercicios/Ejercicio_19-Decimal a binario/Ejercicio19.java
@@ -1,13 +1,16 @@
 //Nombre: Villanueva Aguilar Carlos Raymundo 
-//D�a: 13/02/2019
+//Día: 13/02/2019
 //Hora: 14:07
-import java.util.Scanner;
+import java.util.Scanner;//Se importa la librería scanner para poder utilizar funciones de entrada de datos.
+//La clase Ejercicio19 implementa una aplicación que captura dos números en decimal, los convierte a binario y los imprime.
 class Ejercicio19{
-public static void main(String[]args){
-String x;
-Scanner Dec = new Scanner(System.in);
-System.out.println("Inserta un numero decimal");
-x = Dec.nextLine();
+public static void main(String[]args){/*Se crea el método main para que el programa pueda ejecutarse y se le asignan valores
+de disponibilidad de uso, argumentos y valores de retorno.*/
+String x;//Se asigna una variable tipo string.
+Scanner Dec = new Scanner(System.in);//Se crea un nuevo scanner y se le asigna el nombre de "Dec".
+System.out.println("Inserta un numero decimal");//Se utiliza el comando para imprimit en pantalla y se escribe una instrucción.
+x = Dec.nextLine();//Se llama al scanner para capturar el texto de la proxima linea que se escriba y asignarlo como valor de x.
 System.out.println("Al convertirse a binario el numero insertado es: "
 +Integer.toString(Integer.parseInt(x,10),2));
+ //El texto es convertido a número decimal con el Integer.parseInt y luego es convertido a binario con el Integer.toString.
 }}
