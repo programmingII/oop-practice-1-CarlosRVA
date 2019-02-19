9025acf7fa46b2a07554d62857c89fdf3b155308
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-19 09:02:42 -0700

    Update Ejercicio22.java

--- Ejercicios/Ejercicio_22-Binario a Decimal/Ejercicio22.java
+++ Ejercicios/Ejercicio_22-Binario a Decimal/Ejercicio22.java
@@ -1,13 +1,17 @@
 //Nombre: Villanueva Aguilar Carlos Raymundo 
-//D�a: 15/02/2019
+//Día: 15/02/2019
 //Hora: 09:33
-import java.util.Scanner;
+import java.util.Scanner;//Se importa la libreria scanner para utilizar funciones de entrada de datos.
+//La clase Ejercicio22 implementa una aplicación que recibe un número binario, lo convierte a decimal y lo imprime en pantalla.
 class Ejercicio22{
-public static void main(String[]args){
-Scanner num = new Scanner(System.in);
-String x;
-System.out.println("Inserta un numero binario");
-x = num.nextLine();
+public static void main(String[]args){/*Se escribe el método main para que el programa pueda ejecutarse y se le asignan atributos
+de disponibilidad de uso, valor de retorno y argumentos.*/
+Scanner num = new Scanner(System.in);//Se crea un nuevo scanner y se le asigna el nombre de "num".
+String x;//Se declara una nueva variable tipo string.
+System.out.println("Inserta un numero binario");//Se utiliza el comando para imprimir en pantalla y se escribe una instrucción.
+x = num.nextLine();//Se llama al scanner para almacenar en x a la próxima linea de texto que se inserte.
 System.out.println("Al convertirse a decimal, el numero es: "+
 Integer.toString(Integer.parseInt(x,2),10));
+/*El texto escrito es convertido a número utilizando el Integer.parseInt, para luego ser convertido a número decimal con el Integer.toString,
+ posteriormente es impreso en pantalla.*/
 }}
