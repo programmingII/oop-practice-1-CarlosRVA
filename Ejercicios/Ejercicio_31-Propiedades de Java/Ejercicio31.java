ccb82acf411ba8bb22e2c169fa8dd5d2b36a0f41
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-20 22:09:33 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_31-Propiedades de Java/Ejercicio31.java
@@ -0,0 +1,12 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 20/02/2019
+//Hora: 22:00
+class Ejercicio31{
+public static void main(String[]args){
+System.out.println("Version de Java: " + System.getProperty("java.version"));
+System.out.println("Version de Java Runtime: " + System.getProperty("java.runtime.version"));
+System.out.println("Home de Java: " + System.getProperty("java.home"));
+System.out.println("Vendedor de Java: " + System.getProperty("java.vendor"));
+System.out.println("URL de vendedor de Java: " + System.getProperty("java.vendor.url"));
+System.out.println("Class Path de Java: " + System.getProperty("java.class.path"));
+}}
