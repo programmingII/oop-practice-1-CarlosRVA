008a226f97e582e8a33285f16b25a10eb2e54aab
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-25 21:41:05 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_41-Valor-ASCII-de-un-Caracter/Ejercicio41.java
@@ -0,0 +1,12 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 25/02/2019
+//Hora: 21:30
+import java.util.Scanner;
+class Ejercicio41{
+public static void main(String[]args){
+Scanner S = new Scanner(System.in);
+System.out.println("Escribe un caracter");
+char C = S.nextLine().charAt(0);
+int c = C;
+System.out.println("El ASCII del caracter insertado es: "+c);
+}}
