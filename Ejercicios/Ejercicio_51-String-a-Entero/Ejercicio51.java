f0a8cc0945cd2c09f08c091bbb1ee857b39ec64a
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-26 13:19:28 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_51-String-a-Entero/Ejercicio51.java
@@ -0,0 +1,12 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 2/02/2019
+//Hora: 13:11
+import java.util.Scanner;
+class Ejercicio51{
+public static void main(String[]args){
+Scanner S = new Scanner(System.in);
+System.out.println("Inserta un numero en string");
+String x = S.nextLine();
+int X = Integer.parseInt(x,10);
+System.out.println("El valor entero del numero es: "+X);
+}}
