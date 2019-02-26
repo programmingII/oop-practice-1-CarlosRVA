b4e16f53a5d409cf3cc6c2a6783532b1d0f5aacd
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-26 16:24:58 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_54-Comparación-de-Dígito-Final/Ejercicio54.java
@@ -0,0 +1,21 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 2/02/2019
+//Hora: 16:15
+import java.util.Scanner;
+class Ejercicio54{
+public static void main(String[]args){
+Scanner Num = new Scanner(System.in);
+int a,b,c;
+System.out.println("Escribe el primer numero");
+a = Num.nextInt();
+System.out.println("Escribe el segundo numero");
+b = Num.nextInt();
+System.out.println("Escribe el tercer numero");
+c = Num.nextInt();
+if(a%10==b%10||a%10==c%10||b%10==c%10){
+System.out.println("Al menos dos de los numeros insertados comparten el mismo digito final.");
+}
+else{
+System.out.println("Ninguno de los numeros insertados comparten el mismo digito final.");
+}
+}}
