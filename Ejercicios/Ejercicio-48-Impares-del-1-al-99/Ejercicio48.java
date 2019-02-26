694f76be2dc8fee844cf839f5f45fed596258c31
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-25 22:34:12 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio-48-Impares-del-1-al-99/Ejercicio48.java
@@ -0,0 +1,8 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 25/02/2019
+//Hora: 22:30
+class Ejercicio48{
+public static void main(String[]args){
+for(int x = 1;x<=100;x=x+2){
+System.out.println(x);
+}}}
