31e932db8a4d7acd8d578307832d99fa1d9f4b4f
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-26 09:03:30 -0700

    Update Ejercicio37.java

--- Ejercicios/Ejercicio_37-Reversa de un String/Ejercicio37.java
+++ Ejercicios/Ejercicio_37-Reversa de un String/Ejercicio37.java
@@ -1,14 +1,18 @@
 //Nombre: Villanueva Aguilar Carlos Raymundo 
-//D韆: 22/02/2019
+//D铆a: 22/02/2019
 //Hora: 07:41
-import java.util.Scanner;
+import java.util.Scanner;//Se importa la libreria scanner para poder utilizar funciones de entrada de datos.
+//La clase Ejercicio37 implementa una aplicaci贸n que captura un string, lo invierte y lo imprime en pantalla.
 class Ejercicio37{
-public static void main(String[]args){
-Scanner Pal = new Scanner(System.in);
-System.out.println("Escribe una oracion: ");
-String S = Pal.nextLine();
-StringBuilder Reversa = new StringBuilder(S);
-Reversa.reverse();
-System.out.println("Lo que se escribio fue: "+S);
-System.out.println("Y la reversa es: "+Reversa);
+public static void main(String[]args){/*Se crea el m茅todo main para poder ejecutar el programa y se le asignan los atributos de
+disponibilidad de uso, argumentos y valor de retorno.*/
+Scanner Pal = new Scanner(System.in);//Se llama al scanner y se le asigna el nombre de "Pal".
+System.out.println("Escribe una oracion: ");//Se imprime en pantalla la instrucci贸n de escribir una oraci贸n.
+String S = Pal.nextLine();/*Se declara una variable tipo String y se llama al scanner para almacenar en ella a la pr贸xima l铆nea de
+texto que se inserte.*/
+StringBuilder Reversa = new StringBuilder(S);/*Se crea un nuevo stringbuilder para crear un string que puede ser modificado, y se le
+almacena el valor escrito en S. Se le asigna el nombre de Reversa.*/
+Reversa.reverse();//Se invierte el string almacenado en este StringBuilder.
+System.out.println("Lo que se escribio fue: "+S);//Se imprime el string escrito originalmente.
+System.out.println("Y la reversa es: "+Reversa);//Se imprime la reversa de este string.
 }}
