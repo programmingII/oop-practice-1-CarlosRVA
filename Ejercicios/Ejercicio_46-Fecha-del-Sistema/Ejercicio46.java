a4bd45d096aa786d4f268c9eef81855d5fd407bd
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-25 22:21:12 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_46-Fecha-del-Sistema/Ejercicio46.java
@@ -0,0 +1,7 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 25/02/2019
+//Hora: 22:15
+class Ejercicio46{
+public static void main(String[]args){
+System.out.format("Fecha Actual: %tc%n",System.currentTimeMillis());
+}}
