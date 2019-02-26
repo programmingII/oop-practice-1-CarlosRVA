cf388bf24e00a4dbd8113d6d8bbdc414ce85a510
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-25 21:46:02 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_42-Contraseña/Ejercicio42.java
@@ -0,0 +1,11 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 25/02/2019
+//Hora: 21:41
+import java.util.Scanner;
+class Ejercicio42{
+public static void main(String[]args){
+Scanner S = new Scanner(System.in);
+System.out.println("Escribe una contraseña: ");
+String C = S.nextLine();
+System.out.println("La contraseña ingresada es: "+C);
+}}
