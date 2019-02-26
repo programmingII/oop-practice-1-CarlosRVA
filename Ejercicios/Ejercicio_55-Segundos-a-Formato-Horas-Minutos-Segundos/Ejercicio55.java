6a2f5941b0370366a833aed0312aeb693bcc0e07
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-26 16:36:04 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_55-Segundos-a-Formato-Horas-Minutos-Segundos/Ejercicio55.java
@@ -0,0 +1,16 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 2/02/2019
+//Hora: 16:29
+import java.util.Scanner;
+class Ejercicio55{
+public static void main(String[]args){
+Scanner Seg = new Scanner(System.in);
+int T,H,M,S;
+System.out.println("Escribe una cantidad de segundos");
+T = Seg.nextInt();
+H=T/3600;
+M=(T%3600)/60;
+S=(T%3600)%60;
+System.out.println(H+":"+M+":"+S);
+}
+}
