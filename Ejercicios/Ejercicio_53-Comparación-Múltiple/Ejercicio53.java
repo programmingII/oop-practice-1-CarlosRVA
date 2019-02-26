055c471284a256df3f79af614dd154290bab955e
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-26 16:12:00 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_53-Comparación-Múltiple/Ejercicio53.java
@@ -0,0 +1,21 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 2/02/2019
+//Hora: 16:04
+import java.util.Scanner;
+class Ejercicio53{
+public static void main(String[]args){
+Scanner Num = new Scanner(System.in);
+int a,b,c;
+System.out.println("Inserta el primer numero: ");
+a = Num.nextInt();
+System.out.println("Inserta el segundo numero: ");
+b = Num.nextInt();
+System.out.println("Inserta el tercer numero: ");
+c = Num.nextInt();
+if(a<b&&b<c){
+System.out.println("Se cumple con la condicion de que "+a+" sea menor que "+b+" y que "+b+" sea menor que "+c);
+}
+else{
+System.out.println("No se cumple con la condicion de que "+a+" sea menor que "+b+" y que "+b+" sea menor que "+c);
+}
+}}
