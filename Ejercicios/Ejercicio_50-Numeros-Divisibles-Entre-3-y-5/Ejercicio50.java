8c11bfd2845dc7b4c1b2a8d90fc760bcfda44925
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-26 12:45:27 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_50-Numeros-Divisibles-Entre-3-y-5/Ejercicio50.java
@@ -0,0 +1,21 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 2/02/2019
+//Hora: 12:33
+class Ejercicio50{
+public static void main(String[]args){
+System.out.println("Divisibles entre 3: ");
+for(int x=1;x<=100;x++){
+if(x%3==0){
+System.out.print(x+", ");
+}}
+System.out.println("\n\nDivisibles entre 5: ");
+for(int x=1;x<=100;x++){
+if(x%5==0){
+System.out.print(x+", ");
+}}
+System.out.println("\n\nDivisibles entre 3 y 5: ");
+for(int x=1;x<=100;x++){
+if(x%3==0&&x%5==0){
+System.out.print(x+", ");
+}}
+}}
