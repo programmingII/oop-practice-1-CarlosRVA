0f5a5f6ffe9732f1289a552681422c87f0cd8ee1
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-25 22:13:56 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_45-Tamaño-de-una-Fila/Ejercicio45.java
@@ -0,0 +1,8 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 25/02/2019
+//Hora: 22:06
+import java.io.File;
+class Ejercicio45{
+public static void main(String[]args){
+System.out.println("El tamaño de "+new File("Fila.txt")+" es: "+ ("Fila.txt").length()+" Bytes");
+}}
