470d1550609d0b3a1acd9dfe25b2b744666ba642
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-27 16:33:51 -0700

    Update Ejercicio53.java

--- Ejercicios/Ejercicio_53-Comparación-Múltiple/Ejercicio53.java
+++ Ejercicios/Ejercicio_53-Comparación-Múltiple/Ejercicio53.java
@@ -1,21 +1,24 @@
 //Nombre: Villanueva Aguilar Carlos Raymundo 
 //Día: 26/02/2019
 //Hora: 16:04
-import java.util.Scanner;
+import java.util.Scanner;//Se importa la clase Scanner desde el paquete util, esta clase permite utilizar funciones de entrada de datos.
 class Ejercicio53{
-public static void main(String[]args){
-Scanner Num = new Scanner(System.in);
-int a,b,c;
+public static void main(String[]args){/*Se crea el método main para poder ejecutar el programa y se le asignan atributos de
+disponibilidad de uso, valores de retorno y argumentos.*/
+Scanner Num = new Scanner(System.in);//Se crea un nuevo scanner con el nombre de "Num".
+int a,b,c;//Se declaran tres variables de tipo int.
+/*Se imprimen en pantalla tres instrucciones que le piden al usuario insertar un número, después de cada instrucción se le asigna a cada
+variable un valor dependiendo del número entero que se inserte en cada uno.*/
 System.out.println("Inserta el primer numero: ");
 a = Num.nextInt();
 System.out.println("Inserta el segundo numero: ");
 b = Num.nextInt();
 System.out.println("Inserta el tercer numero: ");
 c = Num.nextInt();
-if(a<b&&b<c){
+if(a<b&&b<c){//Si a es menor que b y b es menor que c, se imprime un texto que dice que se cumple la condición
 System.out.println("Se cumple con la condicion de que "+a+" sea menor que "+b+" y que "+b+" sea menor que "+c);
 }
-else{
+else{//En caso de que no, se imprime que no se cumple.
 System.out.println("No se cumple con la condicion de que "+a+" sea menor que "+b+" y que "+b+" sea menor que "+c);
 }
 }}
