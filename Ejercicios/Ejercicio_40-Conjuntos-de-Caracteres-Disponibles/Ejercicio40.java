756a04821c8bf20625bd51caa1e71ea9af006371
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-27 09:57:24 -0700

    Update Ejercicio40.java

--- Ejercicios/Ejercicio_40-Conjuntos-de-Caracteres-Disponibles/Ejercicio40.java
+++ Ejercicios/Ejercicio_40-Conjuntos-de-Caracteres-Disponibles/Ejercicio40.java
@@ -1,10 +1,13 @@
 //Nombre: Villanueva Aguilar Carlos Raymundo 
-//D�a: 25/02/2019
+//Día: 25/02/2019
 //Hora: 21:23
 import java.nio.charset.Charset;
+/*La clase Ejercicio40 implementa una aplicación que muestra los conjuntos de caracteres disponibles.*/
 class Ejercicio40{
-public static void main(String[]args){
-System.out.println("Los conjuntos caracteres que se tienen disponibles son: ");
+public static void main(String[]args){/*Se crea el método main para ejecutar el programa y se le asignan atributos de
+disponibilidad de uso, valor de retorno y argumentos.*/
+System.out.println("Los conjuntos caracteres que se tienen disponibles son: ");/*Se utiliza este comando para imprimir algo en 
+pantalla*/
 for (String S : Charset.availableCharsets().keySet()){
 System.out.println(S);
 }}}
