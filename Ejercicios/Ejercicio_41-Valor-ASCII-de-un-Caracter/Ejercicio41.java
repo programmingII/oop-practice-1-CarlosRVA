ed617c32beca6460673ff81cf1c8594cee4daa26
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-27 14:28:40 -0700

    Update Ejercicio41.java

--- Ejercicios/Ejercicio_41-Valor-ASCII-de-un-Caracter/Ejercicio41.java
+++ Ejercicios/Ejercicio_41-Valor-ASCII-de-un-Caracter/Ejercicio41.java
@@ -8,7 +8,9 @@ public static void main(String[]args){/*Se crea el método main para poder ejecu
 de disponibilidad de uso, valor de retorno y argumentos.*/
 Scanner S = new Scanner(System.in);//Se crea un nuevo scanner y se le asigna el nombre de "S".
 System.out.println("Escribe un caracter");//Se utiliza este comando para imprimir en pabtalla y se escribe una instrucción.
-char C = S.nextLine().charAt(0);
-int c = C;
-System.out.println("El ASCII del caracter insertado es: "+c);
+char C = S.nextLine().charAt(0);/*Se declara la variable C de tipo char y se almacena en ella el caracter en la posición 0
+que se inserte en la siguiente línea*/
+int c = C;//Se declara la variable c tipo int y se le asigna el valor de la variable C.
+System.out.println("El ASCII del caracter insertado es: "+c);/*Cuando el caracter se convierte a tipo de dato entero, se convierte en el
+número que corresponda a su valor en ASCII. Una vez hecho esto, se imprime dicho valor.*/
 }}
