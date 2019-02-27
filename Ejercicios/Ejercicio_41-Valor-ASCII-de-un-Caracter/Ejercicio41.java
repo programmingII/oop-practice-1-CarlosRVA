b5d1e5d26a21be02d3136f01f2a506d48010661e
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-27 09:52:16 -0700

    Update Ejercicio41.java

--- Ejercicios/Ejercicio_41-Valor-ASCII-de-un-Caracter/Ejercicio41.java
+++ Ejercicios/Ejercicio_41-Valor-ASCII-de-un-Caracter/Ejercicio41.java
@@ -1,11 +1,13 @@
 //Nombre: Villanueva Aguilar Carlos Raymundo 
-//D�a: 25/02/2019
+//Día: 25/02/2019
 //Hora: 21:30
-import java.util.Scanner;
+import java.util.Scanner;//Se importa la librería scanner para poder utilizar funciones de entrada de datos.
+//La clase Ejercicio41 implementa una aplicación que captura un caracter e imprime su valor ASCII
 class Ejercicio41{
-public static void main(String[]args){
-Scanner S = new Scanner(System.in);
-System.out.println("Escribe un caracter");
+public static void main(String[]args){/*Se crea el método main para poder ejecutar el programa y se le asignan atributos
+de disponibilidad de uso, valor de retorno y argumentos.*/
+Scanner S = new Scanner(System.in);//Se crea un nuevo scanner y se le asigna el nombre de "S".
+System.out.println("Escribe un caracter");//Se utiliza este comando para imprimir en pabtalla y se escribe una instrucción.
 char C = S.nextLine().charAt(0);
 int c = C;
 System.out.println("El ASCII del caracter insertado es: "+c);
