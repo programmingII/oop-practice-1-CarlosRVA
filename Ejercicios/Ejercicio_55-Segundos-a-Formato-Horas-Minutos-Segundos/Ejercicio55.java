80a29df5a2396ca0dd35e4ea2ef5420d45b3addb
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-27 18:01:55 -0700

    Update Ejercicio55.java

--- Ejercicios/Ejercicio_55-Segundos-a-Formato-Horas-Minutos-Segundos/Ejercicio55.java
+++ Ejercicios/Ejercicio_55-Segundos-a-Formato-Horas-Minutos-Segundos/Ejercicio55.java
@@ -2,6 +2,7 @@
 //Día: 26/02/2019
 //Hora: 16:29
 import java.util.Scanner;//Se importa la clase Scanner desde el paquete util, esta clase permite utilizar funciones de entrada de datos.
+//La clase Ejercicio55 implementa una aplicación que captura un número en segundos y lo convierte a un formato de Horas, Minutos y Segundos.
 class Ejercicio55{
 public static void main(String[]args){/*Se crea el método main para poder ejecutar el programa y se le asignan atributos de
 disponibilidad de uso, valores de retorno y argumentos.*/
