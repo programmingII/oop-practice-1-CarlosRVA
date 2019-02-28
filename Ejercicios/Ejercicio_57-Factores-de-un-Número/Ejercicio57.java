f386f4c0cac1003ade243aa10079ca01144961f0
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-27 18:04:27 -0700

    Update Ejercicio57.java

--- Ejercicios/Ejercicio_57-Factores-de-un-Número/Ejercicio57.java
+++ Ejercicios/Ejercicio_57-Factores-de-un-Número/Ejercicio57.java
@@ -2,6 +2,7 @@
 //Día: 26/02/2019
 //Hora: 18:26
 import java.util.Scanner;//Se importa la clase Scanner desde el paquete util, esta clase permite utilizar funciones de entrada de datos.
+//La clase Ejercicio57 implementa una aplicación que captura un número e imprime la cantidad de factores que tiene.
 class Ejercicio57{
 public static void main(String[]args){/*Se crea el método main para poder ejecutar el programa y se le asignan atributos de
 disponibilidad de uso, valores de retorno y argumentos.*/
