195c1620e06b3e2c1ea283148a7a94b441b3d371
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-27 17:40:26 -0700

    Update Ejercicio61.java

--- Ejercicios/Ejercicio_61-Palabra-Invertida/Ejercicio61.java
+++ Ejercicios/Ejercicio_61-Palabra-Invertida/Ejercicio61.java
@@ -1,13 +1,14 @@
 //Nombre: Villanueva Aguilar Carlos Raymundo 
-//D�a: 27/02/2019
+//Día: 27/02/2019
 //Hora: 05:15
-import java.util.Scanner;
+import java.util.Scanner;//Se importa la clase Scanner desde el paquete util, esta clase permite utilizar funciones de entrada de datos.
 class Ejercicio61{
-public static void main(String[]args){
-Scanner S = new Scanner(System.in);
-System.out.println("Escribe una palabra");
-String P = S.nextLine();
-StringBuilder R = new StringBuilder(P);
-R.reverse();
-System.out.println("La palabra invertida es: "+R);
+public static void main(String[]args){/*Se crea el método main para poder ejecutar el programa y se le asignan atributos de
+disponibilidad de uso, valores de retorno y argumentos.*/
+Scanner S = new Scanner(System.in);//Se crea un nuevo scanner con el nombre de "S".
+System.out.println("Escribe una palabra");//Se imprime en pantalla la instrucción que le pide al usuario escribir una palabra
+String P = S.nextLine();//Se declara la variable P y se almacena en ella la próxima línea de texto que se inserte.
+StringBuilder R = new StringBuilder(P);//Se crea un nuevo StringBuilder y almacena el valor insertado en P
+R.reverse();//El String almacenado en R es invertido con el método .reverse.
+System.out.println("La palabra invertida es: "+R);//Finalmente se imprime el valor en R, el cual es la palabra original en reversa.
 }}
