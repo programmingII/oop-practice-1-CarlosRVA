8b52e8492c427c6ad18f7ee1dbcce092dd46081e
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-27 18:06:16 -0700

    Update Ejercicio59.java

--- Ejercicios/Ejercicio_59-String-a-Mayúscula/Ejercicio59.java
+++ Ejercicios/Ejercicio_59-String-a-Mayúscula/Ejercicio59.java
@@ -2,7 +2,7 @@
 //Día: 26/02/2019
 //Hora: 18:56
 import java.util.Scanner;//Se importa la clase Scanner desde el paquete util, esta clase permite utilizar funciones de entrada de datos.
-//La clase Ejercicio53 implementa una aplicación que captura un string, transforma sus caracteres a mayúscula y lo imprime.
+//La clase Ejercicio58 implementa una aplicación que captura un string, transforma sus caracteres a mayúscula y lo imprime.
 class Ejercicio59{
 public static void main(String[]args){/*Se crea el método main para poder ejecutar el programa y se le asignan atributos de
 disponibilidad de uso, valores de retorno y argumentos.*/
