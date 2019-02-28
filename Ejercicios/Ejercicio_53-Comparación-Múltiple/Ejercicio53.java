8c108e747002dfac3da9f26a676f25e0a43ab6d5
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-27 17:58:57 -0700

    Update Ejercicio53.java

--- Ejercicios/Ejercicio_53-Comparación-Múltiple/Ejercicio53.java
+++ Ejercicios/Ejercicio_53-Comparación-Múltiple/Ejercicio53.java
@@ -2,6 +2,7 @@
 //Día: 26/02/2019
 //Hora: 16:04
 import java.util.Scanner;//Se importa la clase Scanner desde el paquete util, esta clase permite utilizar funciones de entrada de datos.
+//La clase Ejercicio53 implementa una aplicación que captura 3 números, los compara, e imprime si se cumple o no la condición establecida.
 class Ejercicio53{
 public static void main(String[]args){/*Se crea el método main para poder ejecutar el programa y se le asignan atributos de
 disponibilidad de uso, valores de retorno y argumentos.*/
