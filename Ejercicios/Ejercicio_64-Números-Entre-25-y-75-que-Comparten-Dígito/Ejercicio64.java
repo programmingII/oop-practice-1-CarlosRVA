debb1918ecf1706d76f0a159c1e258781312fd82
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-27 18:10:26 -0700

    Update Ejercicio64.java

--- Ejercicios/Ejercicio_64-Números-Entre-25-y-75-que-Comparten-Dígito/Ejercicio64.java
+++ Ejercicios/Ejercicio_64-Números-Entre-25-y-75-que-Comparten-Dígito/Ejercicio64.java
@@ -2,6 +2,7 @@
 //Día: 26/02/2019
 //Hora: 05:43
 import java.util.Scanner;//Se importa la clase Scanner desde el paquete util, esta clase permite utilizar funciones de entrada de datos.
+//La clase Ejercicio53 implementa una aplicación que captura 2 números, los compara e indica si coinciden o no en algún dígito.
 class Ejercicio64{
 public static void main(String[]args){/*Se crea el método main para poder ejecutar el programa y se le asignan atributos de
 disponibilidad de uso, valores de retorno y argumentos.*/
