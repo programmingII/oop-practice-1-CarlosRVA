4fab79a3ac1ec2e6c929d5f1c660d9a589d0e8d7
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-02-27 20:18:35 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_69-Mitad-de-un-String/Ejercicio69.java
@@ -0,0 +1,12 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 27/02/2019
+//Hora: 20:17
+import java.util.Scanner;
+class Ejercicio69{
+public static void main(String[]args){
+String A;
+Scanner S = new Scanner(System.in);
+System.out.println("Inserta una sentencia");
+A = S.nextLine();
+System.out.println(A.substring(0,A.length()/2));
+}}
