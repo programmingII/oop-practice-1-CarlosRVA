b824ac2e080b7a86641986f0bbc5cb516d69da15
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-03-06 18:19:23 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_89-Administrador-de-Seguridad-del-Sistema/Ejercicio89.java
@@ -0,0 +1,8 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 06/03/2019
+//Hora: 18:09
+class Ejercicio89{
+public static void main(String[]args){
+System.out.println("El administrador actual de seguridad es: ");
+System.out.println(System.getSecurityManager());
+}}
