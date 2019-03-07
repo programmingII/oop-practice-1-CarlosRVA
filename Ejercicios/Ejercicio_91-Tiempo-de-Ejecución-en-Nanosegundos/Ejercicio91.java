5f1a8bf82d035d0f94d271c0df83da8cf41b0450
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-03-06 18:24:15 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_91-Tiempo-de-Ejecución-en-Nanosegundos/Ejercicio91.java
@@ -0,0 +1,7 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 06/03/2019
+//Hora: 18:21
+class Ejercicio91{
+public static void main(String[]args){
+System.out.println("El tiempo que se tarda el programa en ejecutarse es de: "+System.nanoTime()+" nanosegundos");
+}}
