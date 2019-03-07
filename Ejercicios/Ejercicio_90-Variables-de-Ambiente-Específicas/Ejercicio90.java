0f02cf8a2c9bacf8b0ec0d2a4cb9e870ec179e21
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-03-06 18:19:13 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_90-Variables-de-Ambiente-Específicas/Ejercicio90.java
@@ -0,0 +1,12 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 06/03/2019
+//Hora: 18:14
+class Ejercicio90{
+public static void main(String[]args){
+System.out.println("La ruta de clase es: \n");
+System.out.println(System.getenv("PATH"));
+System.out.println("\nEl directorio de filas temporales es: \n");
+System.out.println(System.getenv("TEMP"));
+System.out.println("\nEl nombre de usuario es: \n");
+System.out.println(System.getenv("USERNAME"));
+}}
