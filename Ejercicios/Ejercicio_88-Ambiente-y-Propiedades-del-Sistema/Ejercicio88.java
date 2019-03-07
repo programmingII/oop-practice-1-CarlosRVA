8a88ba79cbf1f93d84a0d7fabe2c3b93fb0e5cb8
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-03-06 18:07:45 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio_88-Ambiente-y-Propiedades-del-Sistema/Ejercicio88.java
@@ -0,0 +1,10 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 06/03/2019
+//Hora: 18:00
+class Ejercicio88{
+public static void main(String[]args){
+System.out.println("El ambiente actual del sistema es: \n");
+System.out.println(System.getenv());
+System.out.println("\nLas propiedades actuales del sistema son: \n");
+System.out.println(System.getProperties());
+}}
